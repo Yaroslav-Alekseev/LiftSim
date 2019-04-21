ced6852d0bee2c1824d638237fc8b0dc28ec0d12
Yaroslav-Alekseev <yaroslav.newpost@gmail.com>
2019-04-21 20:43:04 +0300

    ButtonPanelController

---
+++ Assets/Scripts/ButtonPanelController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPanelController : MonoBehaviour
+{
+
+    public GameObject LayoutGroup; 
+    public ButtonSetFloor ButtonPrefab;
+    public Transform ButtonStop;
+    public InputField FloorsCount;
+
+
+    private void Awake()
+    {
+        FloorsCount.text = "30";
+        FloorsCount.onValueChanged.AddListener(delegate { FillLayout(); });
+    }
+
+    private void Start()
+    {
+        FillLayout();
+    }
+
+
+    private void FillLayout()
+    {
+        var direction = LiftController.Instance.GetDirection();
+
+        if (direction != Direction.Stopped)
+        {
+            return;
+        }
+
+
+        int Floors = int.Parse(FloorsCount.text);
+
+        if (Floors > 30)
+        {
+            Floors = 30;
+            FloorsCount.text = "30";
+        }
+        else if (Floors <= 0)
+        {
+            Floors = 1;
+            FloorsCount.text = "1";
+        }
+
+
+        foreach (Transform Button in LayoutGroup.transform)
+        {
+            if (Button != ButtonStop)
+            {
+                Destroy(Button.gameObject);
+            }
+        }
+
+
+        for (int i = 1; i <= Floors; i++)
+        {
+            var Button = Instantiate(ButtonPrefab, LayoutGroup.transform);
+            Button.TextScript.text = i.ToString();
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/LiftController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Command
+{
+    FromInside,
+    FromOutside
+}
+
+public enum Direction
+{
+    Up,
+    Down,
+    Stopped
+}
+
+public class LiftController : MonoBehaviour
+{
+
+    public static LiftController Instance;
+    private int _currentFloor;
+    private Direction _liftDirection;
+
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _currentFloor = 1;
+        _liftDirection = Direction.Stopped;
+    }
+
+
+    public void SetNextFloor(int Floor, Command CommandSource)
+    {
+
+        Direction NewDirection = Direction.Stopped;
+
+        int DirectionSign = Floor - _currentFloor;
+
+        if (DirectionSign > 0)
+        {
+            NewDirection = Direction.Up;
+        }
+        else if (DirectionSign < 0)
+        {
+            NewDirection = Direction.Down;
+        }
+
+        
+        if (CommandSource == Command.FromInside || NewDirection == _liftDirection || _liftDirection == Direction.Stopped)
+        {
+            GoToFloor(Floor, NewDirection);
+        }
+
+    }
+
+    private void GoToFloor(int TargetFloor, Direction NewDirection)
+    {
+        if (TargetFloor == _currentFloor && _liftDirection == Direction.Stopped)
+        {
+            OpenDoors();
+        }
+        else
+        {
+            _liftDirection = NewDirection;
+            StartCoroutine(FloorCounter(TargetFloor));
+        }
+    }
+
+    public void StopLift()
+    {
+        StopCoroutine(FloorCounter(0));
+        _liftDirection = Direction.Stopped;
+    }
+
+    private void OpenDoors()
+    {
+        Debug.Log("Двери открываются!");
+    }
+
+    private void CloseDoors()
+    {
+        Debug.Log("Двери закрываются!");
+    }
+
+    private IEnumerator FloorCounter(int TargetFloor)
+    {
+        while (_currentFloor != TargetFloor)
+        {
+            yield return new WaitForSeconds(0.5f) /*speed*/;
+
+            if (TargetFloor > _currentFloor)
+            {
+                _currentFloor++;
+                Debug.Log(_currentFloor); ///
+
+            }
+            else
+            {
+                _currentFloor--;
+                Debug.Log(_currentFloor); ///
+            } 
+
+        }
+
+        StopLift();
+        OpenDoors();
+    }
+
+    public Direction GetDirection()
+    {
+        return _liftDirection;
+    }
+
+
+
+}
